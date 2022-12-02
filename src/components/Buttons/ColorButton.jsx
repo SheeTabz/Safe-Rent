7c2a7ca2e29290578e0beb71b638a3b2e1e51b28
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-02 16:32:23 +0300

    Alter text size

--- src/components/Buttons/ColorButton.jsx
+++ src/components/Buttons/ColorButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ColorButton({data}) {
   return (
     <div className>
-        <button className='lg:w-[200px] font-bold text-white border-solid border-2 border-orange-500  bg-orange-500 hover:bg-transparent hover:text-orange-500 py-3 px-7 rounded-3xl text-xl'>{data}</button>
+        <button className='lg:w-[200px] font-bold text-white border-solid border-2 border-orange-500  bg-orange-500 hover:bg-transparent hover:text-orange-500 py-3 px-7 rounded-3xl '>{data}</button>
     </div>
   )
 }
--- src/components/Buttons/WhiteButton.jsx
+++ src/components/Buttons/WhiteButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function WhiteButton({data}) {
   return (
     <div className>
-        <button className='lg:w-[200px] font-bold text-white border-solid border-2 border-white hover:bg-orange-500  py-3 px-7 rounded-3xl text-xl hover:border-transparent'>{data}</button>
+        <button className='lg:w-[200px] font-bold text-white border-solid border-2 border-white hover:bg-orange-500  py-3 px-7 rounded-3xl  hover:border-transparent'>{data}</button>
     </div>
   )
 }
