4de2b29f6b7f91bb8bafab0ebaa0db7e6d39b9cc
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-02 16:31:19 +0300

    Structure custom json

---
+++ src/components/data.jsx
@@ -0,0 +1,39 @@
+import {BsPiggyBank} from 'react-icons/bs'
+import  {AiOutlineCar, AiOutlineStar} from 'react-icons/ai'
+import {FiMonitor, FiAward} from 'react-icons/fi'
+import {Ri24HoursLine} from 'react-icons/ri'
+
+const services = [
+    {
+        name: 'Fixed Rates',
+        icons: <BsPiggyBank/>,
+        description: 'In omni memoria patriae religionis sunt diri historiarum. Golums, zombies et fasci'
+    },
+    {
+        name: 'Reliable Transfers',
+        icons: <AiOutlineCar/>,
+        description: 'In omni memoria patriae religionis sunt diri historiarum. Golums, zombies et fasci'
+    },
+    {
+        name: '24H Customer Service',
+        icons: <Ri24HoursLine/>,
+        description: 'In omni memoria patriae religionis sunt diri historiarum. Golums, zombies et fasci'
+    },
+    {
+        name: 'Free Cancellation',
+        icons: <FiMonitor/>,
+        description: 'In omni memoria patriae religionis sunt diri historiarum. Golums, zombies et fasci'
+    },
+    {
+        name: 'Award winning Service',
+        icons: <FiAward/>,
+        description: 'In omni memoria patriae religionis sunt diri historiarum. Golums, zombies et fasci'
+    },
+    {
+        name: 'Quality Vehicles',
+        icons: <AiOutlineStar/>,
+        description: 'In omni memoria patriae religionis sunt diri historiarum. Golums, zombies et fasci'
+    }
+]
+
+export {services};
