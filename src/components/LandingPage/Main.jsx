2dc523601ce50c5c17fed9b920c2dc219dd172ad
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-02 12:46:27 +0300

    Main component

--- src/components/LandingPage/Main.jsx
+++ src/components/LandingPage/Main.jsx
@@ -4,8 +4,7 @@ import HeroSection from './HeroSection'
 function Main() {
   return (
     <div className='sm:mx-[20px] lg:mx-[90px]'>
-       
-        
+         
         <HeroSection/>
        
        
