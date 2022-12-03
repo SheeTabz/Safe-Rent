fd453fbc82060e47cdd19edc6dd197b6b4e0e77e
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 12:33:20 +0300

    Create  a template for navigatng to pages

---
+++ src/components/PagesTemplate.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {FaGreaterThan} from 'react-icons/fa';
+
+function PagesTemplate({pageData}) {
+  return (
+    <div className='hero h-[30vh] lg:h-[30vh] text-white '>
+    <div className=' h-full w-full bg-no-repeat bg-cover bg-[url("https://www.glassdoor.com/employers/app/uploads/sites/2/2018/08/officemeeting.jpg")]'>
+    <div className='revie relative flex flex-col justify-center items-center'>
+      <h1 className="text-4xl font-bold ">{pageData}</h1>
+      <div className='temp absolute  bottom-0 flex items-center space-x-3 p-4'>
+         <p>HOME</p>
+         <FaGreaterThan/>
+         <p className='text-orange-500'>{pageData}</p>
+      </div>
+     </div>
+     </div>
+ </div>
+  )
+}
+
+export default PagesTemplate
