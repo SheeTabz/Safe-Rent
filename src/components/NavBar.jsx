d85816c945047a977e9d7e8fc517d9e94681295c
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 13:29:57 +0300

    Add routes to nav bar details

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {navData} from './data'
 
 function NavBar() {
     const data = ["HOME","ABOUT US", "SERVICES", "OUR CARS"]
@@ -9,13 +11,17 @@ function NavBar() {
    </div>
    <div >
     <ul className='flex flex-row space-x-7'>
-        {data.map((header, index) => {
-            return <li className='hover:text-orange-600 text-slate-500 font-bold text-sm' key={index}>{header}</li>
+        {navData.map((header, index) => {
+            return <Link to={header.path}>
+            <li className='hover:text-orange-600 text-slate-500 font-bold text-sm' key={index}>{header.name}</li>
+            </Link>
         })}
     </ul>
    </div>
 <div>
+  <Link to='/login'>
     <button className='text-slate-500 font-bold border-solid border-2 border-orange-600 px-4 py-2 rounded-2xl hover:bg-orange-600 hover:text-white'>Log In</button>
+    </Link>
 </div>
     </div>
   )
