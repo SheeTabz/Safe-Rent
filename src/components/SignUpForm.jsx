db53291831ca4824d424bc2c0104aaddf299f546
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 18:32:47 +0300

    Set form state values and onchange

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -7,14 +7,28 @@ function SignUpForm({onClick}) {
            <div className='border-solid border-2 drop-shadow-md h-full w-[500px] flex flex-col  justify-center items-center space-y-8'>
         <h1 className="font-bold text-3xl">Create an account</h1>
         <p className='text-slate-400'>Please enter your details to get started</p>
-       <form className='form flex flex-col space-y-5' >
+       <form onSubmit={handleSubmit} className='form flex flex-col space-y-5' >
         <input 
         className=''
         type="text" 
-        name="name" 
-        placeholder='Username'/>
-        <input type="text" name="password" placeholder='Password'/>
-        <input type="password" name="password-confirmation" placeholder="Password confirmation"/>
+        name="username" 
+        placeholder='Username'
+        value={formData.username}
+        onChange={handleChange}/>
+
+        <input 
+        type="password" 
+        name="password" 
+        placeholder='Password'
+        value={formData.password}
+        onChange={handleChange}/>
+
+        <input 
+        type="password"
+         name="password_confirmation" 
+         placeholder="Password confirmation"
+         value={formData.password_confirmation}
+         onChange={handleChange}/>
         <div className='flex space-x-2'>
         <input type= "checkbox" name="remember"/>
         <label for="remember">Remember me</label>
