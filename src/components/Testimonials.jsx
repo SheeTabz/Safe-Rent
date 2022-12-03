71baf2148e792ddcba227cf9a48df9d848ac2eec
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 12:35:35 +0300

    Refactor testimonials page

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Testimonials() {
+
+  return (
+    <div className='hero h-[50vh] lg:h-[42vh] text-white mt-[150px]'>
+    <div className=' h-full w-full bg-no-repeat bg-cover bg-[url("https://www.glassdoor.com/employers/app/uploads/sites/2/2018/08/officemeeting.jpg")]'>
+    <div className='revie '>
+      
+     </div>
+     </div>
+ </div>
+  )
+}
+
+export default Testimonials
