12cdaa34edd437f8404e9a33d3c864e303077df3
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 13:29:23 +0300

    Add path routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
-import PagesTemplate from './components/PagesTemplate';
 import CarsPage from './pages/CarsPage';
 import LandingPage from './pages/LandingPage';
 import LogInPage from './pages/LogInPage';
@@ -9,9 +9,11 @@ function App() {
   return (
     <div className="App">
   <NavBar/>
-  {/* <LandingPage/> */}
-  <LogInPage/>
-{/* <CarsPage/> */}
+  <Routes>
+    <Route exact path='/' element={<LandingPage/>}/>
+  <Route path='/login' element={<LogInPage/>} />
+  <Route path='/cars' element={<CarsPage/>} />
+</Routes>
     </div>
   );
 }
