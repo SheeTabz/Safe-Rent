ea32b042e1a4f283180a3c296c64025737b9fa58
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 18:35:46 +0300

    UseEffect to rember a logged in user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
@@ -11,14 +11,29 @@ function App() {
 const [logInPath, setLogInPath] = useState('/login')
 
 
+useEffect(()=> {
+  fetch("/me")
+  .then(res => {
+    if(res.ok){
+      res.json().then(user => setUser(user))
+    }
+    else{
+      res.json().then(user => console.log(user.errors))
+    }
+  })
+},[])
 
-
+function handleUser(user) {
+  setUser(user);
+}
+// if(!user) return <LogInPage handleLogin={setUser} />;
+console.log(user)
   return (
     <div className="App">
-  <NavBar user={user}/>
+  <NavBar user={user} setUser={setUser}/>
   <Routes>
     <Route exact path='/' element={<LandingPage/>}/>
-  <Route path='/login' element={<LogInPage/>} />
+  <Route path='/login' element={<LogInPage handleUser={handleUser}/>} />
   <Route path='/cars' element={<CarsPage/>} />
 </Routes>
     </div>
