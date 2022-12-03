2004fc886c6bc5eee5f2463b99684992e57f97be
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 14:21:29 +0300

    Set login state

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -3,8 +3,10 @@ import Footer from '../components/Footer'
 import LogInForm from '../components/LogInForm'
 import SignUpForm from '../components/SignUpForm'
 
-function LogInPage() {
+function LogInPage({setLogInPath}) {
     const [showLogin, setShowLogin] = useState(true);
+    // if (showLogin) { setLogInPath('/login') }
+    // else {  setLogInPath('/signup'); }
   return (
     <div>
         {showLogin ? (
