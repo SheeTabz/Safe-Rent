4d864bd54974b2e1a5fe8aeec9f229a523b652b9
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-04 14:35:37 +0300

    Alter states and cfunctions

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import NavBar from './components/NavBar';
 import CarsPage from './pages/CarsPage';
 import LandingPage from './pages/LandingPage';
 import LogInPage from './pages/LogInPage';
+import SingleCarPage from './pages/SingleCarPage';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState();
+  const [singleData, setData] = useState({})
+  const [ errors, setErrors] = useState()
 const [logInPath, setLogInPath] = useState('/login')
 
 
@@ -18,10 +21,22 @@ useEffect(()=> {
       res.json().then(user => setUser(user))
     }
     else{
-      res.json().then(user => console.log(user.errors))
+      res.json().then(err => console.log(err.errors))
     }
   })
 },[])
+
+function handleOneCar(id){
+  fetch(`/cars/${id}`)
+  .then(res => {
+    if(res.ok){
+      res.json().then(data => setData(data))
+    }
+    else {
+      res.json().then(data => setErrors(data.errors))
+    }
+  })
+}
 
 function handleUser(user) {
   setUser(user);
@@ -34,7 +49,8 @@ console.log(user)
   <Routes>
     <Route exact path='/' element={<LandingPage/>}/>
   <Route path='/login' element={<LogInPage handleUser={handleUser}/>} />
-  <Route path='/cars' element={<CarsPage/>} />
+  <Route path='/cars' element={<CarsPage handleOneCar={handleOneCar}/>} />
+  <Route path='/cars/:id' element={<SingleCarPage data={singleData} user={user}  errors={errors}/>} />
 </Routes>
     </div>
   );
