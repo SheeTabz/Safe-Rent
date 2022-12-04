661e9227ad1eb10a11fa7069d1047ca019691233
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-04 14:32:52 +0300

    Component to fetch and display cars

--- src/pages/CarsPage.jsx
+++ src/pages/CarsPage.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CarList from '../components/CarList'
+import Footer from '../components/Footer'
 import PagesTemplate from '../components/PagesTemplate'
 
 
-function CarsPage() {
+function CarsPage({handleOneCar}) {
+  const [cars, setCars] = useState([])
+ 
+
+useEffect(() => {
+  fetch('/cars')
+  .then(res => {
+    if(res.ok){
+      res.json().then(data => setCars(data))
+    }
+    else {
+      res.json().then(err => console.log(err.errors))
+    }
+  })
+}, [])
+
+
+
   return (
     <>
     <PagesTemplate pageData="OUR CARS"/>
-    <CarList
-    />
+    <CarList cars={cars} onClick={handleOneCar} />
+    <Footer/>
     </>
    
   )
