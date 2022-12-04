1fe5b70a01c1f36838662105847a057c027abc15
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-04 14:34:33 +0300

    Single card component

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 function CarCard({type, level, condition, transition, clas, price, image}) {
   return (
+    
     <div className=' border-solid border-2 border-gray-300 h-[500px] w-[400px]'>
   <div>
     <div>
-        <img src={image} alt='Car'/>
+        <img src={image} alt='Car' className='w-full'/>
     </div>
     
-  <div className='p-5'>
-  <h1 className=' text-3xl font-bold border-b-solid border-b-2 border-b-gray-500 pb-4'>{type}</h1>
+  <div className='px-5 pt-2'>
+  <h1 className=' text-2xl font-bold border-b-solid border-b-2 border-b-gray-500 pb-4'>{type}</h1>
   </div>
         
         <div className='flex justify-between p-5 ' >
@@ -27,13 +28,10 @@ function CarCard({type, level, condition, transition, clas, price, image}) {
             <label for='class' className='font-light'>Transition:</label>
             <p className='font-bold'>{transition}</p>
         </div>
-        <div className='flex space-x-2 text-sm'>
-            <label for='class' className='font-light'>Class:</label>
-            <p className='font-bold'>{clas}</p>
-        </div>
+     
     </div>
     <div className='flex  justify-center items-center'>
-        <p><span className='font-bold text-4xl text-orange-500'>{price}</span>/day</p>
+        <p><span className='font-bold text-4xl text-orange-500'>${price}</span>/day</p>
     </div>
     </div>
   </div>
