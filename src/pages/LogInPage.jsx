f27b80ee3cdad21040fc115ddc058511d323976b
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-05 16:00:59 +0300

    Pass user props

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -3,14 +3,14 @@ import Footer from '../components/Footer'
 import LogInForm from '../components/LogInForm'
 import SignUpForm from '../components/SignUpForm'
 
-function LogInPage({handleUser}) {
+function LogInPage({handleUser, user}) {
     const [showLogin, setShowLogin] = useState(true);
     // if (showLogin) { setLogInPath('/login') }
     // else {  setLogInPath('/signup'); }
   return (
     <div>
         {showLogin ? (
- <LogInForm onClick={() => setShowLogin(false)} handleUser={handleUser}/>
+ <LogInForm onClick={() => setShowLogin(false)} handleUser={handleUser} user={user}/>
         ) : (
 <SignUpForm onClick={() => setShowLogin(true)} handleUser={handleUser}/>
         )}
